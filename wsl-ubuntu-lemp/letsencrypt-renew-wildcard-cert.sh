6df49e162a917708dccacce687e432c19c790276
Jordan Pakrosnis <jpak@jordanpak.com>
2025-08-17 16:50:09 -0400

    Create letsencrypt-renew-wildcard-cert.sh

---
+++ wsl-ubuntu-lemp/letsencrypt-renew-wildcard-cert.sh
@@ -0,0 +1,7 @@
+# @see https://dev.to/nabbisen/let-s-encrypt-renew-wildcard-certificate-manually-with-certbot-1jp
+
+# Delete existing wildcard certificate.
+sudo certbot delete
+
+# Create new certificate.
+sudo certbot certonly --manual --manual-public-ip-logging-ok --preferred-challenges dns-01 --server https://acme-v02.api.letsencrypt.org/directory -d "*.local.jordanpak.com" 
